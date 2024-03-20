621055a52b6b52f9c98c65861a469d13d4fe67c3
Azur <azur@turquoise.dog>
2024-03-20 17:53:53 -0400

    add Movie entity

---
+++ src/main/java/ca/sheridancollege/koenen/finalproject/entities/Movie.java
@@ -0,0 +1,54 @@
+package ca.sheridancollege.koenen.finalproject.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.util.Date;
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@NoArgsConstructor
+@Table(name = "movies")
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NonNull
+    private String title;
+    @NonNull
+    private Date releaseDate;
+    @NonNull
+    private String genre;
+    @NonNull
+    private String director;
+    @NonNull
+    private String description;
+    private String posterPath;
+    private String backdropPath;
+
+    // Using @Data is not recommended for JPA entities!
+    // IntelliJ provides a warning and an auto-fix for this.
+    // (Below)
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Movie movie = (Movie) o;
+        return getId() != null && Objects.equals(getId(), movie.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
+}
